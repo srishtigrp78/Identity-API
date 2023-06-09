eccad20ace35eb2de70f4a7c233e8365a13e6dac
Vidyadevi Salunkhe <84471226+Vidyaaa24@users.noreply.github.com>
2023-06-09 14:38:22 +0530

    modified .gitignore file (#4)
    * modified .gitignore file
    
    * Delete target directory
    
    * Update .gitignore
    
    * Delete logs directory

--- deployidentity.bat
+++
@@ -1,5 +0,0 @@
-del C:\AppServer\Server\wildfly-10.1.0.Final\standalone\deployments\identity-0.0.1.war
-timeout 7
-del C:\AppServer\Server\wildfly-10.1.0.Final\standalone\deployments\identity-0.0.1.war.undeployed
-timeout 7
-copy C:\Users\WA875423\.m2\repository\com\iemr\common\identity\identity\0.0.1\identity-0.0.1.war C:\AppServer\Server\wildfly-10.1.0.Final\standalone\deployments\identity-0.0.1.war
