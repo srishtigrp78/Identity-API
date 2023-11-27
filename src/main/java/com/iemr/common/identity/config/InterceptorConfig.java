2ed416897ccf74ea1dacd87af46dcdbf6099997c
bshivani7 <133210639+bshivani7@users.noreply.github.com>
2023-11-27 18:39:26 +0530

    http insecure changes (#39)
    Co-authored-by: Burdhi. Shivani <BU40088124@wipro.com>

--- src/main/java/com/iemr/common/identity/config/InterceptorConfig.java
+++ src/main/java/com/iemr/common/identity/config/InterceptorConfig.java
@@ -35,9 +35,16 @@ public class InterceptorConfig extends WebMvcConfigurerAdapter
 	@Autowired
 	HTTPRequestInterceptor requestInterceptor;
 
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry)
+//	{
+//		registry.addInterceptor(requestInterceptor);
+//	}
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry)
 	{
-		registry.addInterceptor(requestInterceptor);
+		 registry.addInterceptor(new BlockingHttpMethodInterceptor())
+         .addPathPatterns("/**"); 
 	}
 }
