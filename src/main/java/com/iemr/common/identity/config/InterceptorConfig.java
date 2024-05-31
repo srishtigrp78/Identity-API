131f360ff03a53e2f69752b7f13ea5e8085217c9
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2024-05-31 23:20:43 +0530

    Unnessary code

--- src/main/java/com/iemr/common/identity/config/InterceptorConfig.java
+++ src/main/java/com/iemr/common/identity/config/InterceptorConfig.java
@@ -35,6 +35,6 @@ public class InterceptorConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new BlockingHttpMethodInterceptor()).addPathPatterns("/**");
+		registry.addInterceptor(requestInterceptor);
 	}
 }
